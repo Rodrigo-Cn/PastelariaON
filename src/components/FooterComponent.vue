ccd316b47f24ca0d09cd61bfd4776460ef8146fe
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-01-17 19:55:09 -0300

    feat: Adiciona navbar e footer

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="myfooter">
+        <p>Monte o seu pastel.</p>
+        <p>@Copyright 2025. Rodrigo Costa</p>
+    </div>
+</template>
+<script>
+    export default{
+        name:"FooterComponent",
+    }         
+</script>
+<style scoped>
+    .myfooter{
+        background-color: #222;
+        border-top: 4px solid #111;
+        height:200px;
+        color: rgb(216, 188, 148);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <nav id="navbar">
+        <router-link id="router-img" to="/"><img src="/img/logo.png" id="logo"></router-link>
+        <router-link class="route" to="/">Home</router-link>
+        <router-link class="route" to="/order">Pedidos</router-link>
+    </nav>
+</template>
+<script>
+    export default{
+        name:'NavBarComponent'
+    }
+
+</script>
+<style scoped>
+    #navbar{
+        background-color: #222;
+        border-bottom: 4px solid #111;
+        padding-right: 2%;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        height: 12vh;
+    }
+    #logo{
+        width: 100%;
+    }
+    #router-img{
+        padding-right: 2%;
+        width: 8%;
+    }
+    .route{
+        color: rgb(216, 188, 148);
+        text-decoration: none;
+        padding-right: 2%;
+        transition: .5s;
+    }
+    .route:hover{
+        text-decoration: underline;
+    }
+</style>
